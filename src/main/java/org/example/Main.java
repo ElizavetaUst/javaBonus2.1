65c4f68a59e17e30f1b5687832726d86bea8548e
Elizaveta Ustinova <weta.esu@yandex.ru>
2024-02-11 20:56:18 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.io.PrintStream;
+
+public class Main {
+    public static void main(String[] args) {
+        int balance = 100; //начальный баланс
+        int replenishment = 1000; //сумма пополнения
+        int bonus = 100; //стоимость одного бонусного рубля
+
+        if (replenishment <= 1000) {
+            System.out.println(balance + replenishment);
+        }
+        if (replenishment > 1000) {
+            System.out.println(replenishment / bonus + balance + replenishment);
+        }
+    }
+}
